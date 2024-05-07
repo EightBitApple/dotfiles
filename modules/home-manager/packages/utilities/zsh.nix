78bcf89aed926c86484f448f4fdcb8adc5a7e263
The8BitApple <stefanharris16@gmail.com>
2024-05-07 15:09:10 +0100

    Migrated Zsh config to Zsh nix module.

--- modules/home-manager/packages/utilities/zsh.nix
+++ modules/home-manager/packages/utilities/zsh.nix
@@ -6,6 +6,8 @@
   '';
 
   config = lib.mkIf config.zsh.enable {
+    home.packages = with pkgs; [ fzf fd ];
+
     programs.zsh = {
       enable = true;
       enableCompletion = true;
@@ -17,6 +19,55 @@
         path+=('/home/stefan/dotnet/tools')
         path+=('/home/stefan/.config/emacs/bin')
         export PATH
+
+        export FZF_DEFAULT_COMMAND="fd --full-path -E $HOME/hdd $HOME"
+        export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+        export FZF_ALT_C_COMMAND="fd -t d"
+        export DOTNET_CLI_TELEMETRY_OPTOUT=1
+      '';
+
+      initExtra = ''
+        NEWLINE=$'\n'
+        zstyle ':vcs_info:*' formats ' %s(%F{yellow}%b%f)'
+        PROMPT="%B%{$fg[magenta]%}%~%b
+        ❯ "
+
+        if [ -n "$commands [ fzf-share ]" ]; then
+          source "$(fzf-share)/key-bindings.zsh"
+          source "$(fzf-share)/completion.zsh"
+        fi
+
+        # vi mode
+        bindkey -v
+        export KEYTIMEOUT=1
+
+        # Change cursor shape for different vi modes.
+        function zle-keymap-select {
+          if [[ $KEYMAP == vicmd ]] ||
+            [[ $1 = 'block' ]]; then
+            echo -ne '\e[1 q'
+          elif [[ $KEYMAP == main ]] ||
+              [[ $KEYMAP == viins ]] ||
+              [[ $KEYMAP =  "" ]] ||
+              [[ $1 = 'beam' ]]; then
+            echo -ne '\e[5 q'
+          fi
+        }
+        zle -N zle-keymap-select
+        zle-line-init() {
+            zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+            echo -ne "\e[5 q"
+        }
+        zle -N zle-line-init
+        echo -ne '\e[5 q' # Use beam shape cursor on startup.
+        preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+        # Print a newline before prompt renders, expect for the first line of the shell.
+        precmd() {
+            precmd() {
+                echo
+            }
+        }
       '';
     };
   };
