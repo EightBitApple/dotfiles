2f0d1086ae320efc8a77d2050020d9457b424ef4
The8BitApple <stefanharris16@gmail.com>
2024-05-17 20:02:00 +0100

    Tweaked Thinkfan levels.

--- modules/nixos/services/thinkfan.nix
+++ modules/nixos/services/thinkfan.nix
@@ -6,7 +6,19 @@
   '';
 
   config = lib.mkIf config.thinkfan.enable {
-    services.thinkfan.enable = true;
+    services.thinkfan = {
+      enable = true;
+      levels = [
+        [ 0 0 55 ]
+        [ 1 48 60 ]
+        [ 2 50 61 ]
+        [ 3 52 63 ]
+        [ 4 56 65 ]
+        [ 5 59 66 ]
+        [ 6 63 69 ]
+        [ 7 66 32767 ]
+      ];
+    };
     boot.kernelParams = [ "options" "thinkpad_acpi" "fan_control=1" ];
   };
 }
