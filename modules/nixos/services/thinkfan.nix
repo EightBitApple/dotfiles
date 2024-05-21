1b46f189c4f2992e7aaa82f9df39738a1e41ea9c
The8BitApple <stefanharris16@gmail.com>
2024-05-21 23:01:17 +0100

    Removed Thinkfan levels to quieten laptop fans.

--- modules/nixos/services/thinkfan.nix
+++ modules/nixos/services/thinkfan.nix
@@ -8,16 +8,7 @@
   config = lib.mkIf config.thinkfan.enable {
     services.thinkfan = {
       enable = true;
-      levels = [
-        [ 0 0 55 ]
-        [ 1 48 60 ]
-        [ 2 50 61 ]
-        [ 3 52 63 ]
-        [ 4 56 65 ]
-        [ 5 59 66 ]
-        [ 6 63 69 ]
-        [ 7 66 32767 ]
-      ];
+      levels = [ [ 0 0 55 ] [ 1 48 60 ] ];
     };
     boot.kernelParams = [ "options" "thinkpad_acpi" "fan_control=1" ];
   };
