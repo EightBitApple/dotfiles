8b854ab8bf77655fdeb39d608b5d1bb614a6bb28
The8BitApple <stefanharris16@gmail.com>
2024-06-03 22:46:59 +0100

    Refactored Waybar modules and styling.
    - Waybar is now positioned at the top of the screen rather than on the
      left side. Providing more space for modules at the cost of vertical
      screen real estate.
    
    - Added Nerd Font icons to most modules.
    
    - Changed Waybar's styling.

--- modules/home-manager/packages/window-manager/waybar/waybar.nix
+++ modules/home-manager/packages/window-manager/waybar/waybar.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   options = {
@@ -18,38 +23,37 @@
         settings = {
 
           mainBar = {
-            output = [ config.waybarDisplay1 config.waybarDisplay3 ];
+            output = [
+              config.waybarDisplay1
+              config.waybarDisplay3
+            ];
             layer = "top";
-            position = "left";
-            modules-left = [ "hyprland/workspaces" ];
-            modules-center = [ "hyprland/window#vert" ];
+            position = "top";
+            modules-left = [
+              "hyprland/workspaces"
+              "hyprland/window"
+            ];
             modules-right = [
               "wireplumber"
-              "battery#bat0"
+              "battery"
               "battery#bat1"
               "network"
-              "clock#date"
               "clock"
               "tray"
             ];
 
-            "tray" = {
+            "hyprland/window" = {
+              icon = true;
               icon-size = 21;
+            };
+
+            "tray" = {
+              icon-size = 16;
               spacing = 10;
             };
 
             "clock" = {
-              format = ''
-                {:%H
-                %M}'';
-              timezone = "Europe/London";
-            };
-
-            "clock#date" = {
-              format = ''
-                {:%a
-                %b
-                %d}'';
+              format = "{:%a %b %d %H:%M}";
               timezone = "Europe/London";
               tooltip-format = ''
                 <big>{:%Y %B}</big>
@@ -57,30 +61,32 @@
             };
 
             "network" = {
-              format-wifi = ''
-                WLS
-                {signalStrength}'';
-              format-ethernet = "ETH";
+              format-wifi = " {signalStrength}%";
+              format-ethernet = "󰈀";
               tooltip-format = ''
                 {ifname}	{ipaddr}/{cidr}
                 gateway	{gwaddr}
                 	{essid}
                 {icon}  ⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}'';
-              format-linked = "{ifname} (No IP) ";
-              format-disconnected = "DC";
+              format-disconnected = "󰅛";
               on-click = "footclient -e nmtui";
             };
 
-            "battery#bat0" = {
+            "battery" = {
               bat = "BAT0";
               interval = 60;
               states = {
                 warning = 30;
                 critical = 15;
               };
-              format = ''
-                B0
-                {capacity}'';
+              format = "{icon} {capacity}%";
+              format-icons = [
+                ""
+                ""
+                ""
+                ""
+                ""
+              ];
             };
 
             "battery#bat1" = {
@@ -90,9 +96,14 @@
                 warning = 30;
                 critical = 15;
               };
-              format = ''
-                B1
-                {capacity}'';
+              format = "{icon} {capacity}%";
+              format-icons = [
+                ""
+                ""
+                ""
+                ""
+                ""
+              ];
             };
 
             "hyprland/workspaces" = {
@@ -102,148 +113,58 @@
               on-click = "activate";
             };
 
-            "hyprland/window#vert" = {
-              max-length = 64;
-              rotate = 90;
-            };
-
             "wireplumber" = {
-              format = ''
-                VOL
-                {volume}'';
+              format = "{icon} {volume}%";
+              format-muted = "";
               on-click = "footclient pulsemixer";
-            };
-          };
-
-          secondaryBar = {
-            output = [ config.waybarDisplay2 ];
-            layer = "top";
-            position = "bottom";
-            modules-left = [ "hyprland/workspaces" ];
-            modules-center = [ "hyprland/window" ];
-            modules-right = [
-              "wireplumber"
-              "custom/battery"
-              "network"
-              "clock#date"
-              "clock"
-              "tray"
-            ];
-            "tray" = {
-              icon-size = 21;
-              spacing = 10;
-            };
-
-            "clock" = {
-              format = "{:%H:%M}";
-              timezone = "Europe/London";
-            };
-
-            "clock#date" = {
-              format = "{:%a %b %d}";
-              timezone = "Europe/London";
-              tooltip-format = ''
-                <big>{:%Y %B}</big>
-                <tt><small>{calendar}</small></tt>'';
-            };
-
-            "network" = {
-              format-wifi = "WLS {signalStrength}";
-              format-ethernet = "ETH";
-              tooltip-format = ''
-                {ifname}	{ipaddr}/{cidr}
-                gateway	{gwaddr}
-                	{essid}
-                {icon}  ⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}'';
-              format-linked = "{ifname} (No IP) ";
-              format-disconnected = "DC";
-              on-click = "footclient -e nmtui";
-            };
-
-            "custom/battery" = {
-              format = "{}";
-              exec = "~/.local/bin/custom-bar-modules/bm-battery";
-              interval = 60;
-              signal = 10;
-              return-type = "json";
-            };
-
-            "hyprland/workspaces" = {
-              format = "{}";
-              on-scroll-up = "hyprctl dispatch workspace e+1";
-              on-scroll-down = "hyprctl dispatch workspace e-1";
-              on-click = "activate";
-            };
-
-            "hyprland/window" = { max-length = 64; };
-
-            "wireplumber" = {
-              format = "VOL {volume}";
-              on-click = "footclient pulsemixer";
+              format-icons = [
+                ""
+                ""
+                ""
+              ];
             };
           };
         };
         style = with config.colorScheme.palette; ''
           * {
-              /* `otf-font-awesome` is required to be installed for icons */
+              border: none;
+              border-radius: 2px;
               font-family: FiraCode Nerd Font;
-              font-weight: 500;
               font-size: 11px;
+              font-weight: 500;
+              min-height: 0;
           }
 
           /* Waybar */
           window#waybar {
-              background-color: transparent;
-          }
-
-          /* Window Title */
-          #window {
-              color: #${base07};
-              margin: 0.5em;
-              padding: 0.5em;
-          }
-
-          window#waybar.empty .modules-center {
-              background-color: transparent;
-              border-width: 0;
-          }
-
-          .modules-center {
-              margin: 0.5em;
               background-color: #${base00};
-              border-radius: 0.5em;
-              box-shadow: 2px 2px #${base07};
+              border-bottom: 3px solid #${base01};
+              color: #${base07};
+              border-radius: 0;
           }
 
-          window#waybar.empty .modules-center {
-              background-color: transparent;
-              border-width: 0;
-              box-shadow: none;
+          #window {
+              padding-left: 10px;
+              padding-right: 10px;
+              color: #${base04};
           }
 
           #workspaces button {
-              margin: 0.25em 0.5em;
-              padding: 0.25em 0.5em;
+              padding: 0 0.4em;
+              background-color: transparent;
               color: #${base07};
-              background-color: #${base00};
-              border-radius: 0.5em;
-              box-shadow: 2px 2px #${base07};
-          }
-
-          #workspaces#vert button {
-              margin: 0.5em 0.25em;
-              padding: 0.5em 0.25em;
+              box-shadow: inset 0 -3px transparent;
           }
 
           #workspaces button:hover {
               background-color: #${base03};
-              color: #${base06};
+              box-shadow: inset 0 -3px transparent;
           }
 
           #workspaces button.active {
-              color: #${base00};
               background-color: #${base07};
-              box-shadow: 2px 2px #${base00};
+              box-shadow: inset 0 -3px transparent;
+              color: #${base00};
           }
 
           #workspaces button.urgent {
@@ -262,54 +183,33 @@
               box-shadow: 2px 2px;
           }
 
-          #battery, #battery.plugged {
-              color: #${base0D};
-          }
-
           #battery.charging, #battery.plugged {
               color: #${base0B};
           }
 
-          @keyframes blink {
-              to {
-                  background-color: #ffffff;
-                  color: #000000;
-              }
+          #battery.warning {
+              color: #${base0A};
           }
 
           #battery.critical:not(.charging) {
               color: #${base08};
-              animation-name: blink;
-              animation-duration: 0.5s;
-              animation-timing-function: linear;
-              animation-iteration-count: infinite;
-              animation-direction: alternate;
           }
 
           #clock.date,
           #clock,
           #network,
           #wireplumber,
-          #battery.bat0,
+          #battery,
           #battery.bat1,
           #tray {
-              margin: 0.25em 0.5em;
-              padding: 0.25em 0.5em;
-              background-color: #${base04};
-              color: #${base00};
-              border-radius: 0.5em;
-              box-shadow: 2px 2px #${base00};
-          }
-
-          #tray {
-              background-color: #${base00};
-              box-shadow: 2px 2px #${base07};
-              padding: 0.5em;
+            padding: 0 10px;
+            margin: 6px 2px;
+            color: #${base04};
           }
 
           #wireplumber.muted,
           #network.disconnected {
-              background-color: #${base03};
+              color: #${base03};
           }
 
           #tray > .passive {
