e3e449562d6819d441319cd7ad07e337d1daa05b
The8BitApple <stefanharris16@gmail.com>
2024-06-06 15:27:34 +0100

    Added Bluetooth module.

--- modules/home-manager/packages/window-manager/waybar/waybar.nix
+++ modules/home-manager/packages/window-manager/waybar/waybar.nix
@@ -38,6 +38,7 @@
               "battery#controller"
               "battery#bat0"
               "battery#bat1"
+              "bluetooth"
               "network"
               "clock"
               "tray"
@@ -140,6 +141,14 @@
                 ""
               ];
             };
+            "bluetooth" = {
+              format = "󰂯 {status}";
+              format-off = ""; # an empty format will hide the module
+              format-connected = "󰂱 {num_connections}";
+              tooltip-format = "{controller_alias}\t{controller_address}";
+              tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{device_enumerate}";
+              tooltip-format-enumerate-connected = "{device_alias}\t{device_address}";
+            };
           };
         };
         style = with config.colorScheme.palette; ''
@@ -216,6 +225,7 @@
           #battery.controller,
           #battery.bat0,
           #battery.bat1,
+          #bluetooth,
           #tray {
             padding: 0 10px;
             margin: 6px 2px;
