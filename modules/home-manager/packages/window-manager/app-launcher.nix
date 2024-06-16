4af987672cff4d4e54b2f9843d17e7ee144081a8
The8BitApple <stefanharris16@gmail.com>
2024-06-16 14:47:45 +0100

    Modified tofi module to use home-manager options.

--- modules/home-manager/packages/window-manager/app-launcher.nix
+++ modules/home-manager/packages/window-manager/app-launcher.nix
@@ -13,23 +13,8 @@
   config = lib.mkIf config.appLauncher.enable {
     home.packages = with pkgs; [ tofi ];
 
-    home.file.".config/tofi/config".text = with config.stylix.base16Scheme; ''
-      text-color = #${base07}
-      selection-color = #${base0B}
-
-      font = monospace
-      corner-radius = 2
-      outline-color = #${base01}
-      outline-width = 2
-      border-color = #${base02}
-      border-width = 2
-      background-color = #${base00}
-      text-color = #${base07}
-      selection-color = #${base0B}
-      prompt-text = "run: "
-      num-results = 8
-      width = 640
-      height = 480
-    '';
+    programs.tofi = {
+      enable = true;
+    };
   };
 }
