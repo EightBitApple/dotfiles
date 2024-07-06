3c735ff61db2e34b1fc22b60ca6fa60816b8a1dc
The8BitApple <stefanharris16@gmail.com>
2024-07-06 13:17:43 +0100

    Refactored laptop toggle display script.
    The script will now fetch the display name rather than hardcoding it. A
    notification will also be displayed telling the user that the display is
    about to turn off. The notification sound when turning on the display
    will signal that it's about to turn on.

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -8,17 +8,27 @@
 let
   toggleLaptopDisplay = pkgs.writeShellApplication {
     name = "toggle-display";
-    runtimeInputs = with pkgs; [ jq ];
+    runtimeInputs = with pkgs; [
+      jq
+      dunst
+    ];
     text = ''
-      display_status=$(hyprctl -j monitors | jq '.[0].dpmsStatus')
-      sleep 1
+      display_json=$(hyprctl -j monitors | jq '.[0]')
 
-      if [ "$display_status" = "true" ]
-      then
-        hyprctl dispatch dpms off eDP-1
-      else
-        hyprctl dispatch dpms on eDP-1
-      fi
+      sleep_delay=1
+      notif_time=2000
+
+      # Get monitor info as JSON and parse it.
+      display_status=$(printf "%s" "$display_json" | jq '.dpmsStatus')
+      display_name=$(printf "%s" "$display_json" | jq -r '.name')
+
+      new_status=""
+      [ "$display_status" = "true" ] && new_status="off" || new_status="on"
+
+      # Turn display on or off based on outcome of branch.
+      notify-send -t "$notif_time" "Turning $new_status laptop display..."
+      sleep "$sleep_delay"
+      hyprctl dispatch dpms "$new_status" "$display_name"
     '';
   };
 in
