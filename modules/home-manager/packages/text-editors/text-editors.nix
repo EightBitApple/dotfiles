508c101ac9ddeea4c6123bb05063a79ba99d5afe
EightBitApple <stefanharris16@gmail.com>
2024-07-21 17:15:07 +0100

    Added packages for Nix LSP and treesitter.

--- modules/home-manager/packages/text-editors/text-editors.nix
+++ modules/home-manager/packages/text-editors/text-editors.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, inputs, userSettings, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  userSettings,
+  ...
+}:
 
 {
   options.textEditors.enable = lib.mkEnableOption ''
@@ -15,53 +21,55 @@
       neovim.enable = true;
     };
 
-    home.packages = with pkgs;
-      [
-        (pkgs.writeShellApplication {
-          name = "emc";
-          runtimeInputs = with pkgs; [ dunst ];
-          text = ''
-            # A simple script to notify the user that an Emacs server is starting, and that
-            # the frame they just opened is a client.
-            # The user can also pass file paths to this script for emacsclient to open.
+    home.packages = with pkgs; [
+      python3
+      nixd
 
-            startEmacsServer() {
-              notify 3000 "No server running. Starting a server now."
-              systemctl --user start emacs.service
-              notify 3000 "Server started running."
-            }
+      (writeShellApplication {
+        name = "emc";
+        runtimeInputs = [ dunst ];
+        text = ''
+          # A simple script to notify the user that an Emacs server is starting, and that
+          # the frame they just opened is a client.
+          # The user can also pass file paths to this script for emacsclient to open.
 
-            restartEmacsServer() {
-              notify 3000 "Restarting running server."
-              emacsclient -e '(kill-emacs)'
-              startEmacsServer;
-              systemctl --user restart emacs.service
-              startClient;
-            }
+          startEmacsServer() {
+            notify 3000 "No server running. Starting a server now."
+            systemctl --user start emacs.service
+            notify 3000 "Server started running."
+          }
 
-            startClient() {
-              notify 1000 "Starting the client."
-              emacsclient -c &
-            }
+          restartEmacsServer() {
+            notify 3000 "Restarting running server."
+            emacsclient -e '(kill-emacs)'
+            startEmacsServer;
+            systemctl --user restart emacs.service
+            startClient;
+          }
 
-            notify() {
-              duration="$1"
-              title="Emacs Client"
-              message="$2"
+          startClient() {
+            notify 1000 "Starting the client."
+            emacsclient -c &
+          }
 
-              notify-send -u low -t "$duration" "$title" "$message" &
-            }
+          notify() {
+            duration="$1"
+            title="Emacs Client"
+            message="$2"
 
-            for arg in "$@"; do
-              if [ "$arg" = "-r" ]; then
-                restartEmacsServer
-                exit 0;
-              fi
-            done
+            notify-send -u low -t "$duration" "$title" "$message" &
+          }
 
-            startClient
-          '';
-        })
-      ];
+          for arg in "$@"; do
+            if [ "$arg" = "-r" ]; then
+              restartEmacsServer
+              exit 0;
+            fi
+          done
+
+          startClient
+        '';
+      })
+    ];
   };
 }
