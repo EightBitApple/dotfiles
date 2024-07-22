5f2a10c3a7c46181cbb592785486456e4af3f0cb
EightBitApple <stefanharris16@gmail.com>
2024-07-22 19:07:18 +0100

    Removed thinkfan option from desktop config.

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -3,7 +3,6 @@
 {
   networking.hostName = lib.mkForce "desktop";
   imports = [ ./hardware-configuration.nix ];
-  thinkfan.enable = false;
   brillo.enable = false;
   tlp.enable = false;
   intelHwdec.enable = false;
