c6dd5417fb688b91324f7e40a5344e733df1a9cf
EightBitApple <stefanharris16@gmail.com>
2024-08-16 23:21:41 +0100

    Added listener's to Hypridle.

--- modules/home-manager/packages/window-manager/hypr/hypridle.nix
+++ modules/home-manager/packages/window-manager/hypr/hypridle.nix
@@ -21,6 +21,18 @@
           before_sleep_cmd = "${pkgs.hyprlock}/bin/hyprlock";
           ignore_dbus_inhibit = false;
         };
+
+        listener = [
+          {
+            timeout = 300;
+            on-timeout = "${pkgs.hyprlock}/bin/hyprlock";
+          }
+          {
+            timeout = 500;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+        ];
       };
     };
   };
