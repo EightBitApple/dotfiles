4eace4959ffc4e86bade6a9831dad7addf86680c
EightBitApple <stefanharris16@gmail.com>
2024-08-17 18:31:38 +0100

    Override intel-vaapi-driver.
    Libva 2.22 broke hardware decoding via intel-vaapi-driver since the
    latter has not yet made wl_drm optional. There is an open pull request
    whose commit I've added to an override.
    https://github.com/intel/intel-vaapi-driver/pull/566

--- modules/nixos/hardware/intel-hwdec.nix
+++ modules/nixos/hardware/intel-hwdec.nix
@@ -14,13 +14,19 @@
     hardware.graphics = {
       enable = true;
       extraPackages = with pkgs; [
-        intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-        libvdpau-va-gl
+        (intel-vaapi-driver.overrideAttrs {
+          src = fetchFromGitHub {
+            owner = "intel";
+            repo = "intel-vaapi-driver";
+            rev = "4206d0e15363d188f30f2f3dbcc212fef206fc1d";
+            hash = "sha256-kbasiVOz9eHbO87SEuVMDT2pK5ouiVGglL+wcFJH9IM=";
+          };
+        })
       ];
     };
+
     environment.sessionVariables = {
       LIBVA_DRIVER_NAME = "i965";
-    }; # Force intel-vaapi-driver
+    }; # Force intel-vaapi-driver (older but works better for Firefox/Chromium)
   };
 }
