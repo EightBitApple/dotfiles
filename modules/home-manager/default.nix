8116c3750503c497f1e4b537699c8a40fbc55abb
EightBitApple <stefanharris16@gmail.com>
2024-08-28 18:15:02 +0100

    Sorted default.nix files in alphabetical order.

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -3,97 +3,84 @@
 {
   imports = [
     ./fonts.nix
-    ./users/users.nix
 
-    ./packages/text-editors/text-editors.nix
     ./packages/browsers/firefox.nix
-    ./packages/terminals.nix
-    ./packages/programming-tools.nix
-
-    ./packages/media/video-playback.nix
+    ./packages/gaming/mangohud.nix
+    ./packages/gaming/sourceports/dsda-doom.nix
+    ./packages/gaming/sourceports/gzdoom.nix
+    ./packages/gaming/sourceports/ironwail.nix
     ./packages/media/audio-tools.nix
-
-    ./packages/window-manager/waybar/waybar.nix
-
-    ./packages/window-manager/app-launcher.nix
-    ./packages/window-manager/dunst/dunst.nix
-    ./packages/window-manager/clipboard.nix
-    ./packages/window-manager/night-light.nix
-    ./packages/window-manager/theming.nix
-
-    ./packages/window-manager/hypr/hyprpaper.nix
-    ./packages/window-manager/hypr/hyprland.nix
-    ./packages/window-manager/hypr/hyprlock.nix
-    ./packages/window-manager/hypr/hypridle.nix
-
+    ./packages/media/video-playback.nix
+    ./packages/programming-tools.nix
+    ./packages/terminals.nix
+    ./packages/text-editors/text-editors.nix
     ./packages/utilities/extra-utilities.nix
     ./packages/utilities/git.nix
     ./packages/utilities/home-manager.nix
     ./packages/utilities/htop.nix
-    ./packages/utilities/zsh.nix
     ./packages/utilities/syncthing.nix
+    ./packages/utilities/zsh.nix
+    ./packages/window-manager/app-launcher.nix
+    ./packages/window-manager/clipboard.nix
+    ./packages/window-manager/dunst/dunst.nix
+    ./packages/window-manager/hypr/hypridle.nix
+    ./packages/window-manager/hypr/hyprland.nix
+    ./packages/window-manager/hypr/hyprlock.nix
+    ./packages/window-manager/hypr/hyprpaper.nix
+    ./packages/window-manager/night-light.nix
+    ./packages/window-manager/theming.nix
+    ./packages/window-manager/waybar/waybar.nix
 
-    ./packages/gaming/sourceports/dsda-doom.nix
-    ./packages/gaming/sourceports/gzdoom.nix
-    ./packages/gaming/sourceports/ironwail.nix
-
-    ./packages/gaming/mangohud.nix
+    ./resources/resources.nix
 
     ./scripts/scripts.nix
-    ./resources/resources.nix
+
+    ./users/users.nix
   ];
 
-  fonts.enable = lib.mkDefault true;
-  users.enable = lib.mkDefault true;
-
-  textEditors.enable = lib.mkDefault true;
-  firefox.enable = lib.mkDefault true;
-  terminals.enable = lib.mkDefault true;
-  programmingTools.enable = lib.mkDefault true;
-
-  videoPlayback.enable = lib.mkDefault true;
-  audioTools.enable = lib.mkDefault true;
-
-  waybar.enable = lib.mkDefault true;
-
   appLauncher.enable = lib.mkDefault true;
-  dunst.enable = lib.mkDefault true;
+  audioTools.enable = lib.mkDefault true;
   clipboard.enable = lib.mkDefault true;
-  nightLight.enable = lib.mkDefault true;
-  theming.enable = lib.mkDefault true;
-
-  hyprpaper.enable = lib.mkDefault true;
-  hyprland.enable = lib.mkDefault true;
-  hyprlock.enable = lib.mkDefault true;
-  hypridle.enable = lib.mkDefault true;
-
+  dsda-doom.enable = lib.mkDefault true;
+  dunst.enable = lib.mkDefault true;
   extra-utilities.enable = lib.mkDefault true;
+  firefox.enable = lib.mkDefault true;
+  fonts.enable = lib.mkDefault true;
   git.enable = lib.mkDefault true;
+  gzdoom.enable = lib.mkDefault true;
   home-manager.enable = lib.mkDefault true;
   htop.enable = lib.mkDefault true;
-  zsh.enable = lib.mkDefault true;
-  syncthing.enable = lib.mkDefault true;
-
-  dsda-doom.enable = lib.mkDefault true;
-  gzdoom.enable = lib.mkDefault true;
+  hypridle.enable = lib.mkDefault true;
+  hyprland.enable = lib.mkDefault true;
+  hyprlock.enable = lib.mkDefault true;
+  hyprpaper.enable = lib.mkDefault true;
   ironwail.enable = lib.mkDefault true;
-
   mangohud.enable = lib.mkDefault true;
-
-  scripts.enable = lib.mkDefault true;
+  nightLight.enable = lib.mkDefault true;
+  programmingTools.enable = lib.mkDefault true;
   resources.enable = lib.mkDefault true;
+  scripts.enable = lib.mkDefault true;
+  syncthing.enable = lib.mkDefault true;
+  terminals.enable = lib.mkDefault true;
+  textEditors.enable = lib.mkDefault true;
+  theming.enable = lib.mkDefault true;
+  users.enable = lib.mkDefault true;
+  videoPlayback.enable = lib.mkDefault true;
+  waybar.enable = lib.mkDefault true;
+  zsh.enable = lib.mkDefault true;
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
-
-  home.sessionVariables = {
-    EDITOR = "emacs";
-    FLAKE = ".dotfiles";
+  home = {
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "23.11"; # Please read the comment before changing.
+    sessionVariables = {
+      EDITOR = "emacs";
+      FLAKE = ".dotfiles";
+    };
   };
 }
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -4,97 +4,90 @@
   imports = [
     ./boot/bootloader.nix
 
+    ./console/keymap.nix
+
+    ./hardware/bluetooth.nix
+    ./hardware/brillo.nix
+    ./hardware/intel-hwdec.nix
+    ./hardware/microcode-updates.nix
+
     ./kernel/hid/apple-hid.nix
     ./kernel/memory/zram.nix
     ./kernel/packages/rtl88xxau-aircrack.nix
 
-    ./services/sound/pipewire.nix
-    ./services/device/printing.nix
-    ./services/security/pam.nix
-    ./services/device/fstrim.nix
-    ./services/networking/dnscrypt.nix
-    ./services/filesystem/gvfs.nix
-    ./services/filesystem/tumbler.nix
-    ./services/device/udev.nix
-    ./services/daemon/emacs-daemon.nix
-    ./services/device/tlp.nix
-    ./services/display/ly.nix
-
-    ./networking/network.nix
     ./networking/firewall.nix
+    ./networking/network.nix
 
     ./nix/experimental-features.nix
+
+    ./packages/pkgs-unfree.nix
+
+    ./programs/dconf.nix
+    ./programs/gaming.nix
+    ./programs/git.nix
+    ./programs/hyprland.nix
+    ./programs/thunar.nix
+    ./programs/wireshark.nix
+    ./programs/zsh.nix
+
+    ./services/daemon/emacs-daemon.nix
+    ./services/device/fstrim.nix
+    ./services/device/logind.nix
+    ./services/device/printing.nix
+    ./services/device/tlp.nix
+    ./services/device/udev.nix
+    ./services/display/ly.nix
+    ./services/filesystem/gvfs.nix
+    ./services/filesystem/tumbler.nix
+    ./services/networking/dnscrypt.nix
+    ./services/security/pam.nix
+    ./services/sound/pipewire.nix
 
     ./system/locale.nix
     ./system/mount-drive.nix
     ./system/virtualisation.nix
 
-    ./hardware/bluetooth.nix
-    ./hardware/microcode-updates.nix
-    ./hardware/brillo.nix
-    ./hardware/intel-hwdec.nix
-
-    ./console/keymap.nix
-
-    ./users/groups.nix
     ./users/accounts.nix
-
-    ./packages/pkgs-unfree.nix
-
-    ./programs/git.nix
-    ./programs/zsh.nix
-    ./programs/gaming.nix
-    ./programs/wireshark.nix
-    ./programs/thunar.nix
-    ./programs/hyprland.nix
-    ./programs/dconf.nix
+    ./users/groups.nix
   ];
 
-  bootloader.enable = lib.mkDefault true;
-
+  accounts.enable = lib.mkDefault true;
   apple-hid.enable = lib.mkDefault true;
-  zram.enable = lib.mkDefault true;
-  rtl88xxau-aircrack.enable = lib.mkDefault true;
-
-  pipewire.enable = lib.mkDefault true;
-  printing.enable = lib.mkDefault true;
-  pam.enable = lib.mkDefault true;
-  fstrim.enable = lib.mkDefault true;
+  bluetooth.enable = lib.mkDefault true;
+  bootloader.enable = lib.mkDefault true;
+  brillo.enable = lib.mkDefault true;
+  dconf.enable = lib.mkDefault true;
   dnscrypt.enable = lib.mkDefault true;
+  emacsDaemon.enable = lib.mkDefault true;
+  experimental-features.enable = lib.mkDefault true;
+  firewall.enable = lib.mkDefault true;
+  fstrim.enable = lib.mkDefault true;
+  gaming.enable = lib.mkDefault true;
+  git.enable = lib.mkDefault true;
+  groups.enable = lib.mkDefault true;
   gvfs.enable = lib.mkDefault true;
+  hyprland.enable = lib.mkDefault true;
+  intelHwdec.enable = lib.mkDefault true;
+  keymap.enable = lib.mkDefault true;
+  locale.enable = lib.mkDefault true;
+  logind.enable = lib.mkDefault true;
+  ly.enable = lib.mkDefault true;
+  microcode-updates.enable = lib.mkDefault true;
+  mount-drive.enable = lib.mkDefault true;
+  network.enable = lib.mkDefault true;
+  pam.enable = lib.mkDefault true;
+  pipewire.enable = lib.mkDefault true;
+  pkgs-unfree.enable = lib.mkDefault true;
+  printing.enable = lib.mkDefault true;
+  rtl88xxau-aircrack.enable = lib.mkDefault true;
+  thunar.enable = lib.mkDefault true;
+  tlp.enable = lib.mkDefault true;
   tumbler.enable = lib.mkDefault true;
   udev.enable = lib.mkDefault true;
-
-  network.enable = lib.mkDefault true;
-  firewall.enable = lib.mkDefault true;
-
-  experimental-features.enable = lib.mkDefault true;
-  locale.enable = lib.mkDefault true;
-  mount-drive.enable = lib.mkDefault true;
   virtualisation.enable = lib.mkDefault true;
-
-  bluetooth.enable = lib.mkDefault true;
-  microcode-updates.enable = lib.mkDefault true;
-  brillo.enable = lib.mkDefault true;
-  intelHwdec.enable = lib.mkDefault true;
-
-  keymap.enable = lib.mkDefault true;
-
-  groups.enable = lib.mkDefault true;
-  accounts.enable = lib.mkDefault true;
-
-  pkgs-unfree.enable = lib.mkDefault true;
-
-  git.enable = lib.mkDefault true;
-  zsh.enable = lib.mkDefault true;
-  gaming.enable = lib.mkDefault true;
   wireshark.enable = lib.mkDefault true;
-  thunar.enable = lib.mkDefault true;
-  hyprland.enable = lib.mkDefault true;
-  dconf.enable = lib.mkDefault true;
-  emacsDaemon.enable = lib.mkDefault true;
-  tlp.enable = lib.mkDefault true;
-  ly.enable = lib.mkDefault true;
+  zram.enable = lib.mkDefault true;
+  zsh.enable = lib.mkDefault true;
 
   system.stateVersion = "23.11";
 }
