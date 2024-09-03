1c22643d385c784c9d2eb639e61b8e238888ebac
EightBitApple <stefanharris16@gmail.com>
2024-09-03 15:28:46 +0100

    Tweaked TLP.
    - Tweaked start and stop charge thresholds to improve battery health.
    - Tweaked CPU profiles to reduce fan noise.

--- modules/nixos/services/device/tlp.nix
+++ modules/nixos/services/device/tlp.nix
@@ -5,5 +5,25 @@
     Enable and configure the tlp power management service.
   '';
 
-  config = lib.mkIf config.tlp.enable { services.tlp.enable = true; };
+  config = lib.mkIf config.tlp.enable {
+    services.tlp = {
+      enable = true;
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+
+        START_CHARGE_THRESH_BAT1 = 75;
+        STOP_CHARGE_THRESH_BAT1 = 80;
+
+        RUNTIME_PM_ON_AC = "auto";
+        RUNTIME_PM_ON_BAT = "auto";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+
+        WIFI_PWR_ON_AC = "on";
+        WIFI_PWR_ON_BAT = "on";
+      };
+    };
+  };
 }
