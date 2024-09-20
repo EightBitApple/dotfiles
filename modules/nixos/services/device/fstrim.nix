8b4867c9abe84cbb72406dc3e03abf1898ba33b1
EightBitApple <stefanharris16@gmail.com>
2024-09-20 11:30:20 +0100

    Changed fstrim to run every Tuesday.

--- modules/nixos/services/device/fstrim.nix
+++ modules/nixos/services/device/fstrim.nix
@@ -1,9 +1,19 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  systemSettings,
+  ...
+}:
 
 {
   options.fstrim.enable = lib.mkEnableOption ''
     Enable fstrim to trim SSDs on a weekly bases.
   '';
 
-  config = lib.mkIf config.fstrim.enable { services.fstrim.enable = true; };
+  config = lib.mkIf config.fstrim.enable {
+    services.fstrim = {
+      enable = true;
+      interval = "${systemSettings.maintenanceDay}";
+    };
+  };
 }
