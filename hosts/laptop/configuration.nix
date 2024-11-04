cf881c4e3649aa0a7a5af01a34ecc80f4f1a2b5e
EightBitApple <stefanharris16@gmail.com>
2024-11-04 21:18:57 +0000

    Enabled loading of Thinkpad ACPI kernel modules.
    Seems to fix the issue of fan not starting after waking from suspend.

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -18,5 +18,12 @@
   plasma6.enable = false;
   printing.enable = false;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [
+      "options"
+      "thinkpad_acpi"
+      "fan_control=1"
+    ];
+  };
 }
