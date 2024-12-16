aef6c07ade94516256114aa678d38542a39281c5
EightBitApple <stefanharris16@gmail.com>
2024-12-16 15:33:22 +0000

    Removed 'config' option from home-manager module.

--- modules/home-manager/packages/utilities/home-manager.nix
+++ modules/home-manager/packages/utilities/home-manager.nix
@@ -1,8 +1,5 @@
 { config, lib, ... }:
 
 {
-  options.home-manager.enable = lib.mkEnableOption ''
-    Enable home-manager to configure itself.
-  '';
-  config = lib.mkIf config.home-manager.enable { home-manager.enable = true; };
+  home-manager.enable = true;
 }
