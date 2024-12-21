7ee27211ffc2e4c20b9138c7ba14ac506b871331
EightBitApple <stefanharris16@gmail.com>
2024-12-21 20:08:52 +0000

    Removed config option from resource expression.

--- modules/home-manager/resources/resources.nix
+++ modules/home-manager/resources/resources.nix
@@ -1,15 +1,7 @@
-{ config, lib, ... }:
-
 {
-  options.resources.enable = lib.mkEnableOption ''
-    Enable the use of resources.
-  '';
-
-  config = lib.mkIf config.resources.enable {
-    home.file = {
-      ".local/share/soundfonts".source = ./content/soundfonts;
-      ".local/share/wallpapers".source = ./content/wallpapers;
-      ".local/share/system-sounds".source = ./content/system-sounds;
-    };
+  home.file = {
+    ".local/share/soundfonts".source = ./content/soundfonts;
+    ".local/share/wallpapers".source = ./content/wallpapers;
+    ".local/share/system-sounds".source = ./content/system-sounds;
   };
 }
