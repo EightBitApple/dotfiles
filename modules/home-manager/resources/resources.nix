7374eeb357c05e053718fa6e1ad21879ed96e153
EightBitApple <stefanharris16@gmail.com>
2024-12-21 20:16:30 +0000

    Edited file paths in resources expression.

--- modules/home-manager/resources/resources.nix
+++ modules/home-manager/resources/resources.nix
@@ -1,7 +1,7 @@
 {
   home.file = {
-    ".local/share/soundfonts".source = ./content/soundfonts;
-    ".local/share/wallpapers".source = ./content/wallpapers;
-    ".local/share/system-sounds".source = ./content/system-sounds;
+    ".local/share/soundfonts".source = ./soundfonts;
+    ".local/share/wallpapers".source = ./wallpapers;
+    ".local/share/system-sounds".source = ./system-sounds;
   };
 }
