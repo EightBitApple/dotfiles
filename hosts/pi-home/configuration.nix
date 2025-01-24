644ff2c0fc7289684e059f3aace33f38473a5ebe
EightBitApple <stefanharris16@gmail.com>
2025-01-24 17:19:39 +0000

    Disabled more modules; enabled rtl88xxau module.

--- hosts/pi-home/configuration.nix
+++ hosts/pi-home/configuration.nix
@@ -10,10 +10,26 @@
   imports = [ ./hardware-configuration.nix ];
 
   disabledModules = [
-    ../../modules/nixos/kernel/drivers/rtl88xxau-aircrack.nix
+    ../../modules/nixos/hardware/brillo.nix
+    ../../modules/nixos/hardware/intel-hwdec.nix
+    ../../modules/nixos/kernel/hid/apple-hid.nix
     ../../modules/nixos/programs/gaming.nix
+    ../../modules/nixos/programs/hyprland.nix
     ../../modules/nixos/programs/theming.nix
+    ../../modules/nixos/programs/thunar.nix
+    ../../modules/nixos/programs/wireshark.nix
+    ../../modules/nixos/programs/zsh.nix
     ../../modules/nixos/services/device/printing.nix
+    ../../modules/nixos/services/device/tlp.nix
+    ../../modules/nixos/services/device/udev.nix
+    ../../modules/nixos/services/filesystem/gvfs.nix
+    ../../modules/nixos/services/filesystem/tumbler.nix
+    ../../modules/nixos/services/networking/dnscrypt.nix
+    ../../modules/nixos/services/networking/network-manager.nix
+    ../../modules/nixos/services/networking/ssh-agent.nix
+    ../../modules/nixos/services/security/gnupg.nix
+    ../../modules/nixos/services/security/physlock.nix
+    ../../modules/nixos/services/sound/pipewire.nix
     ../../modules/nixos/system/mount-drive.nix
     ../../modules/nixos/users/accounts.nix
   ];
