dff9da5a89e9053f2f7122eb3bb7d032d6349e09
EightBitApple <stefanharris16@gmail.com>
2025-02-14 17:58:11 +0000

    Changed icon pack source to forked repo.
    Since the broken symbolic link is an upstream issue - and upstream seems
    abandoned - I forked the repo and fixed the symlink.

--- modules/home-manager/packages/window-manager/theming.nix
+++ modules/home-manager/packages/window-manager/theming.nix
@@ -7,7 +7,12 @@
       name = "Obsidian-Green";
       package = pkgs.iconpack-obsidian.overrideAttrs (
         final: prev: {
-          dontCheckForBrokenSymlinks = true;
+          src = pkgs.fetchFromGitHub {
+            owner = "EightBitApple";
+            repo = "iconpack-obsidian";
+            rev = "b4d3d75724f48a8d1f22a46a52ce324362487ff3";
+            sha256 = "7G2XDS49CZgQqbnUADHWyw9Ko8kGuFkFWc0fqHR7xgE=";
+          };
         }
       );
     };
