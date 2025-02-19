652f4a1eb9832fd52fed2ae791f579881da50b42
EightBitApple <stefanharris16@gmail.com>
2025-02-19 15:26:06 +0000

    Removed GameMode GPU settings.

--- modules/nixos/programs/gaming.nix
+++ modules/nixos/programs/gaming.nix
@@ -11,11 +11,6 @@
       enable = true;
       enableRenice = true;
       settings = {
-        # Warning: GPU optimisations have the potential to damage hardware
-        gpu = {
-          gpu_device = 0;
-          amd_performance_level = "high";
-        };
         custom = {
           start = "${pkgs.libnotify}/bin/notify-send -t 3000 'GameMode started.'";
           end = "${pkgs.libnotify}/bin/notify-send -t 3000 'GameMode ended.'";
