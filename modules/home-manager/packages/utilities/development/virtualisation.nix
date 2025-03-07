12c95afb84181217712eec23f8b363f7797efa84
EightBitApple <stefanharris16@gmail.com>
2025-03-07 20:10:10 +0000

    Moved virtualisation dev tools to HM module.

--- modules/home-manager/packages/utilities/development/virtualisation.nix
+++ modules/home-manager/packages/utilities/development/virtualisation.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ quickemu ];
+  home.packages = with pkgs; [
+    quickemu
+    dive           # look into docker image layers
+    podman-tui     # status of containers in the terminal
+    podman-compose # start group of containers for dev
+    distrobox      # use any linux distro in your terminal
+  ];
 }
--- modules/nixos/system/virtualisation.nix
+++ modules/nixos/system/virtualisation.nix
@@ -15,13 +15,4 @@
       defaultNetwork.settings.dns_enabled = true;
     };
   };
-
-  # Useful other development tools
-  environment.systemPackages = with pkgs; [
-    dive # look into docker image layers
-    podman-tui # status of containers in the terminal
-    podman-compose # start group of containers for dev
-
-    distrobox # use any linux distro in your terminal
-  ];
 }
