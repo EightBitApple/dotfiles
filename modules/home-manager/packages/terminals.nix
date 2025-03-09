76ab5dbd31279313a97f3bb163d6421e4e46844b
EightBitApple <stefanharris16@gmail.com>
2025-03-09 13:36:29 +0000

    Added transparency to Alacritty.

--- modules/home-manager/packages/terminals.nix
+++ modules/home-manager/packages/terminals.nix
@@ -2,9 +2,12 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      window.padding = {
-        x = 4;
-        y = 4;
+      window = {
+        padding = {
+          x = 4;
+          y = 4;
+        };
+        opacity = 0.9;
       };
     };
   };
