1f15870b1cde9b70d70854f8ec22b3029c649c5a
EightBitApple <stefanharris16@gmail.com>
2025-03-13 13:03:18 +0000

    Virtualisation module cleanup.

--- modules/nixos/system/virtualisation.nix
+++ modules/nixos/system/virtualisation.nix
@@ -1,19 +1,13 @@
-{ pkgs, ... }:
-
 {
-  # From https://wiki.nixos.org/wiki/Podman
-  # Enable common container config files in /etc/containers
   virtualisation = {
     containers.enable = true;
     spiceUSBRedirection.enable = true;
+
+    # From https://wiki.nixos.org/wiki/Podman
     podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
+      enable = true; # Enable common container config files in /etc/containers
+      dockerCompat = true; # Create a `docker` alias for podman, to use it as a drop-in replacement
+      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
     };
   };
 }
