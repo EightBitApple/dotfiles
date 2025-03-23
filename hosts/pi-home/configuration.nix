d9dc82c4c2097edaf718b73bd237cb21f2bb8b56
EightBitApple <stefanharris16@gmail.com>
2025-03-23 22:15:38 +0000

    Removed mount module from pi-home disabled list.
    Since it no longer exists.

--- hosts/pi-home/configuration.nix
+++ hosts/pi-home/configuration.nix
@@ -40,7 +40,6 @@
     ../../modules/nixos/services/security/gnupg.nix
     ../../modules/nixos/services/sound/pipewire.nix
     ../../modules/nixos/system/bootloader.nix
-    ../../modules/nixos/system/mount-drive.nix
     ../../modules/nixos/system/virtualisation.nix
   ];
 
