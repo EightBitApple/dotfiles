728d98c06d6f8665b6596037a3e5cac484b59813
EightBitApple <stefanharris16@gmail.com>
2025-03-31 20:51:34 +0100

    Tweaked zram memory percentage and swappiness.

--- modules/nixos/kernel/memory/zram.nix
+++ modules/nixos/kernel/memory/zram.nix
@@ -1,15 +1,14 @@
 {
-  # Enables zram and sets it to double that of physical RAM
-
   zramSwap = {
     enable = true;
-    memoryPercent = 200;
+    memoryPercent = 150;
   };
 
   # Tweaking the system's swap to take full advantage of zram.
   # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
+  # https://github.com/NixOS/nixpkgs/pull/351002
   boot.kernel.sysctl = {
-    "vm.swappiness" = 180;
+    "vm.swappiness" = 150;
     "vm.watermark_boost_factor" = 0;
     "vm.watermark_scale_factor" = 125;
     "vm.page-cluster" = 0;
