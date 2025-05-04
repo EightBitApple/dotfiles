cdd977579426f14a2afe7a75eab3604ea466797f
EightBitApple <stefanharris16@gmail.com>
2025-05-04 21:51:10 +0100

    Forced home-pi to use 5GHz WiFi by setting BSSID.
    'freq_list' was dropping connection presumably when the router changed
    WiFi channel. After reading a post on Snapcraft suggesting to connect
    via BSSID instead, I decided to give that a try. [1]
    
    I replaced the SSID secret with one titled 'BSSID' which uses the BSSID
    of the 5GHz band. The PSK has to be a base64 encoded password derived
    from both the SSID and ASCII passsword, which can be calculated using
    the command 'wpa_passphrase <SSID> <password>'. [2]
    
    [1] https://forum.snapcraft.io/t/provide-wpa-supplicant-conf-to-networkmanager-to-disable-5ghz/29598
    [2] https://raspberrypi.stackexchange.com/a/139389

--- hosts/pi-home/overrides/wpa-supplicant.nix
+++ hosts/pi-home/overrides/wpa-supplicant.nix
@@ -3,7 +3,7 @@
 {
   sops = {
     secrets = {
-      "wpa/network1/SSID" = {};
+      "wpa/network1/BSSID" = {};
       "wpa/network1/psk" = {};
     };
 
@@ -13,9 +13,8 @@
     templates."wpa_supplicant.conf" = {
       content = ''
         network={
-          ssid="${config.sops.placeholder."wpa/network1/SSID"}"
-          psk="${config.sops.placeholder."wpa/network1/psk"}"
-          freq_list=5160 5180 5200 5220 5240 5260 5280 5300 5320 5340 5480 5500 5520 5540 5560 5580 5600 5620 5640 5660 5680 5700 5720 5745 5765 5785 5805 5825 5845 5865
+          bssid=${config.sops.placeholder."wpa/network1/BSSID"}
+          psk=${config.sops.placeholder."wpa/network1/psk"}
         }
       '';
       path = "/etc/wpa_supplicant.conf";
