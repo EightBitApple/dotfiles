73d059624bdfdfd508c3b29373899eb27a0d83b3
EightBitApple <stefanharris16@gmail.com>
2025-05-07 20:43:41 +0100

    Added vertical monitor to Hyprland config.

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -12,8 +12,9 @@
       "$mod ALT, S, exec, hyprctl keyword monitor DP-1,1920x1080@60,0x0,1 & notify-send -t 2000 'Refresh Rate: 60hz'"
     ];
     monitor = [
-      "DP-1,1920x1080@75,0x0,1"
-      "HDMI-A-1,1920x1080@60,0x0,1"
+      "DP-1, 1920x1080@75, 0x0, 1"
+      "DP-2, 1920x1080@60, 1920x-1080, 1, transform, 1"
+      "HDMI-A-1, 1920x1080@60, 0x0, 1"
     ];
 
     cursor = {
