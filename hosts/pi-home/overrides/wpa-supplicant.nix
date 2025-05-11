fb2c192727a0ca142db186914b09df9d66558401
EightBitApple <stefanharris16@gmail.com>
2025-05-11 15:22:15 +0100

    Added bssid_ignore and new freq_list to pi-home.
    pi-home was still connecting to the 2.4Ghz band. 'bssid_ignore' will
    tell wpa_supplicant to ignore the BSSID of the 2.4GHz band [1].
    'bssid_ignore' was renamed from 'bssid_blacklist' in 2021 [2]. For
    insurance 'freq_list' was added back, but this time with frequencies
    listed by the command 'iw list'.
    
    [1] https://superuser.com/a/1324561
    [2] https://w1.fi/cgit/hostap/commit/?id=752b1c60813db84d4158f6d299c89fe68488a812

--- hosts/pi-home/overrides/wpa-supplicant.nix
+++ hosts/pi-home/overrides/wpa-supplicant.nix
@@ -3,7 +3,9 @@
 {
   sops = {
     secrets = {
+      "wpa/network1/SSID" = {};
       "wpa/network1/BSSID" = {};
+      "wpa/network1/BSSID_ignore" = {};
       "wpa/network1/psk" = {};
     };
 
@@ -12,8 +14,11 @@
     # https://github.com/NixOS/nixpkgs/issues/342140#issuecomment-2365125619
     templates."wpa_supplicant.conf" = {
       content = ''
+        freq_list=5170 5180 5190 5200 5210 5220 5230 5240 5260 5280 5300 5320 5500 5520 5540 5560 5580 5600 5620 5640 5660 5680 5700 5720 5745 5765 5785 5805 5825
         network={
+          ssid="${config.sops.placeholder."wpa/network1/SSID"}"
           bssid=${config.sops.placeholder."wpa/network1/BSSID"}
+          bssid_ignore=${config.sops.placeholder."wpa/network1/BSSID_ignore"}
           psk=${config.sops.placeholder."wpa/network1/psk"}
         }
       '';
