991aa2b75513dfe37be99cfc570f8fd90eeae23c
EightBitApple <stefanharris16@gmail.com>
2025-05-23 23:23:01 +0100

    Added Librewolf to pi-home disabled modules.

--- hosts/pi-home/configuration.nix
+++ hosts/pi-home/configuration.nix
@@ -18,6 +18,7 @@
     ../../modules/nixos/programs/emacs.nix
     ../../modules/nixos/programs/gaming.nix
     ../../modules/nixos/programs/hyprland.nix
+    ../../modules/nixos/programs/librewolf.nix
     ../../modules/nixos/programs/searx.nix
     ../../modules/nixos/programs/theming.nix
     ../../modules/nixos/programs/wireshark.nix
