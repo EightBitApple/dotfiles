b881ad48b36f4d0cde24a393ae22ad7278fd81d5
EightBitApple <stefanharris16@gmail.com>
2025-05-24 16:47:52 +0100

    Bumped rtw88 driver to latest commit.
    So it builds for the latest kernel [1][2].
    
    [1] https://www.github.com/lwfinger/rtw88/pull/264
    [2] https://www.github.com/lwfinger/rtw88/commit/3215ac2e3ef91011869407de321df5e62c11617a

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,6 +15,22 @@ in
       };
     })
 
+    (final: prev: {
+      linuxPackages_latest = prev.linuxPackages_latest.extend (
+        lpfinal: lpprev: {
+          rtw88 = prev.linuxPackages_latest.rtw88.overrideAttrs (old: {
+            version = "0-unstable-2025-05-08";
+            src = prev.fetchFromGitHub {
+              owner = "lwfinger";
+              repo = "rtw88";
+              rev = "461b696b51317ba4ca585a4ddb32f2e72cd4efc9";
+              hash = "sha256-PBAON2s7gKvpO40vppahEtkWJ4HIjbOjfUz+fx87gIA=";
+            };
+          });
+        }
+      );
+    })
+
     (final: prev: { mpv = prev.mpv.override { scripts = mpvScripts; }; })
   ];
 }
