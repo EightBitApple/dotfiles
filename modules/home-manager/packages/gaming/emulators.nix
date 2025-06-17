06f173b7db599d97b4a5ccbfe420dcb8f31b0be4
EightBitApple <stefanharris16@gmail.com>
2025-06-17 20:14:05 +0100

    Added Retroarch cores.

--- modules/home-manager/packages/gaming/emulators.nix
+++ modules/home-manager/packages/gaming/emulators.nix
@@ -1,10 +1,19 @@
 { pkgs, ... }:
 
+let
+  retroarchWithCores = (pkgs.retroarch.withCores (cores: with cores; [
+    blastem
+    bsnes
+    mesen
+    mupen64plus
+    swanstation
+  ]));
+in
 {
   home.packages = with pkgs; [
+    retroarchWithCores
     cemu
     dolphin-emu
     pcsx2
-    retroarch
   ];
 }
