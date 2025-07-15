d011744c25e1743ac9e97856623a5e474de08602
EightBitApple <stefanharris16@gmail.com>
2025-07-15 13:57:10 +0100

    Fixed system build: updated gimp and clisp hotfix.
    https://www.github.com/NixOS/nixpkgs/issues/424928#issuecomment-3068026156
    https://www.github.com/NixOS/nixpkgs/issues/425310
    https://www.github.com/NixOS/nixpkgs/pull/425299

--- modules/home-manager/packages/utilities/editing/image.nix
+++ modules/home-manager/packages/utilities/editing/image.nix
@@ -3,7 +3,7 @@
 {
   home = {
     packages = with pkgs; [
-      gimp
+      gimp3
       imagemagick
     ];
 
--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./custom-pkgs
+    ./hotfixes/clisp.nix
     ./mpv-scripts.nix
     ./rtw88/latest.nix
     ./rtw88/lts.nix
