2e81d08eecab982817f4822326a2c40076304ee6
EightBitApple <stefanharris16@gmail.com>
2025-09-20 16:55:17 +0100

    Removed nh auto-cleaning.

--- modules/nixos/nix/nix-helpers.nix
+++ modules/nixos/nix/nix-helpers.nix
@@ -1,14 +1,8 @@
 { userSettings, ... }:
 
 {
-  # Install Nix helpers.
-
   programs.nh = {
     enable = true;
     flake = "${userSettings.user.dotfilesDir}";
-    clean = {
-      enable = true;
-      extraArgs = "--keep 4";
-    };
   };
 }
