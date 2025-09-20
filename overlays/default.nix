21d528b40236e2187897d80bf9f27e6396e4f423
EightBitApple <stefanharris16@gmail.com>
2025-09-20 16:53:49 +0100

    Removed rtw88 driver for LTS kernel.

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@
   imports = [
     ./custom-pkgs
     ./hotfixes/rtw88/latest.nix
-    ./hotfixes/rtw88/lts.nix
     ./mpv-scripts.nix
   ];
 }
--- overlays/hotfixes/rtw88/latest.nix
+++ overlays/hotfixes/rtw88/latest.nix
@@ -1,21 +1,17 @@
 { systemSettings, ... }:
 
-let
-  driver = systemSettings.overlays.rtw88;
-in
-
 {
   nixpkgs.overlays = [
     (final: prev: {
       linuxPackages_latest = prev.linuxPackages_latest.extend (
         lpfinal: lpprev: {
           rtw88 = prev.linuxPackages_latest.rtw88.overrideAttrs (old: {
-            version = driver.version;
+            version = "2025-08-09-hotfix";
             src = prev.fetchFromGitHub {
-              owner = driver.owner;
-              repo = driver.repo;
-              rev = driver.rev;
-              hash = driver.hash;
+              owner = "lwfinger";
+              repo = "rtw88";
+              rev = "52072d874840f28c247b27f5d799f2c5c88a7e61";
+              hash = "sha256-Lu3ol0iKfM9/SJ5lieh4A28h02Stc971ZypfiB2BWJ0=";
             };
           });
         }
