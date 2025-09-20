c2742e1a46aaef6cd0bc47195a72d6cbc3b68d7f
EightBitApple <stefanharris16@gmail.com>
2025-09-20 21:19:16 +0100

    Increased vm.swappiness to 180.

--- modules/nixos/kernel/memory/zram.nix
+++ modules/nixos/kernel/memory/zram.nix
@@ -8,7 +8,7 @@
   # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
   # https://github.com/NixOS/nixpkgs/pull/351002
   boot.kernel.sysctl = {
-    "vm.swappiness" = 150;
+    "vm.swappiness" = 180;
     "vm.watermark_boost_factor" = 0;
     "vm.watermark_scale_factor" = 125;
     "vm.page-cluster" = 0;
