0adf494c72c144b9cd6c37ae3b188c492f14e040
EightBitApple <stefanharris16@gmail.com>
2025-10-04 23:49:59 +0100

    Added 1440p ytdl mpv desktop override.

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -2,6 +2,7 @@
   imports = [
     ../../modules/home-manager
     ../../overlays
+    ./home-manager/packages/media/video-playback.nix
     ./home-manager/packages/window-manager/hyprland/hyprland.nix
   ];
 }
