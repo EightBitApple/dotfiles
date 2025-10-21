c1230738244dbe6b909262acfa49f69f557e07ca
EightBitApple <stefanharris16@gmail.com>
2025-10-21 12:37:25 +0100

    Updated system and hotfixes.

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,9 +1,6 @@
 {
   imports = [
     ./custom-pkgs
-    ./hotfixes/cemu.nix
-    ./hotfixes/cxxopts.nix
-    ./hotfixes/dolphin-emu.nix
     ./hotfixes/swaync.nix
     ./hotfixes/yt-dlp.nix
   ];
--- overlays/hotfixes/yt-dlp.nix
+++ overlays/hotfixes/yt-dlp.nix
@@ -3,13 +3,13 @@
     (final: prev: {
       yt-dlp = prev.yt-dlp.overrideAttrs (old: rec {
         pname = "yt-dlp-hotfix";
-        version = "2025.09.23";
+        version = "2025.10.14";
 
         src = prev.fetchFromGitHub {
           owner = "yt-dlp";
           repo = "yt-dlp";
           tag = version;
-          hash = "sha256-pqdR1JfiqvBs5vSKF7bBBKqq0DRAi3kXCN1zDvaW3nQ=";
+          hash = "sha256-x7vpuXUihlC4jONwjmWnPECFZ7xiVAOFSDUgBNvl+aA=";
         };
       });
     })
