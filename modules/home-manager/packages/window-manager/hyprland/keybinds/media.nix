77a86aac7ed7579810104bbd0d574adc9a0cdd03
EightBitApple <stefanharris16@gmail.com>
2025-10-28 15:31:38 +0000

    Changed volume script to use different icons.

--- modules/home-manager/packages/window-manager/hyprland/keybinds/media.nix
+++ modules/home-manager/packages/window-manager/hyprland/keybinds/media.nix
@@ -3,19 +3,19 @@
 {
   wayland.windowManager.hyprland.settings = {
     bind = [
-      "$mod CTRL, H, exec, changevolume mute"
-      ", XF86AudioMute, exec, changevolume mute"
+      "$mod CTRL, H, exec, change-volume mute"
+      ", XF86AudioMute, exec, change-volume mute"
       ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_SOURCE@ toggle"
 
-      "$mod CTRL, K, exec, changevolume up 5"
-      "$mod CTRL ALT, K, exec, changevolume up 1"
-      ", XF86AudioRaiseVolume, exec, changevolume up 5"
-      "SHIFT, XF86AudioRaiseVolume, exec, changevolume up 1"
+      "$mod CTRL, K, exec, change-volume up 5"
+      "$mod CTRL ALT, K, exec, change-volume up 1"
+      ", XF86AudioRaiseVolume, exec, change-volume up 5"
+      "SHIFT, XF86AudioRaiseVolume, exec, change-volume up 1"
 
-      "$mod CTRL, J, exec, changevolume down 5"
-      "$mod CTRL ALT, J, exec, changevolume down 1"
-      ", XF86AudioLowerVolume, exec, changevolume down 5"
-      "SHIFT, XF86AudioLowerVolume, exec, changevolume down 1"
+      "$mod CTRL, J, exec, change-volume down 5"
+      "$mod CTRL ALT, J, exec, change-volume down 1"
+      ", XF86AudioLowerVolume, exec, change-volume down 5"
+      "SHIFT, XF86AudioLowerVolume, exec, change-volume down 1"
       "$mod, S, pass, class:^(com\.obsproject\.Studio)$"
       "$mod, S, exec, sh obs-clip"
     ];
