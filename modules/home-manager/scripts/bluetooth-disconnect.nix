8961f27541175d88fbef25700939fc3bdea697a8
EightBitApple <stefanharris16@gmail.com>
2025-10-28 23:52:31 +0000

    Added new icons for Bluetooth notifications.

--- modules/home-manager/scripts/bluetooth-disconnect.nix
+++ modules/home-manager/scripts/bluetooth-disconnect.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
 
 let
+  icon = {
+    path = "${pkgs.myPackages.oxylite-icon-theme}/share/icons/Oxylite/status/bluetooth-";
+    aquiring = "${icon.path}acquiring.svg";
+    disconnected = "${icon.path}disconnected.svg";
+  };
+
   notif = {
     title = "Bluetooth Disconnect";
-    icon = "${pkgs.myPackages.oxylite-icon-theme}/share/icons/Oxylite/devices/bluetooth.svg";
     hint = "string:x-canonical-private-synchronous:bluetooth_dev";
     time = "2000";
   };
@@ -22,7 +27,7 @@ in
       text = ''
         devices=$(bluetoothctl devices Connected)
         if [ ! "$devices" ]; then
-           notify-send -h ${notif.hint} -i ${notif.icon} -t ${notif.time} "${notif.title}" "No devices connected."
+           notify-send -h ${notif.hint} -i ${icon.disconnected} -t ${notif.time} "${notif.title}" "No devices connected."
            exit 1
         fi
 
@@ -32,9 +37,9 @@ in
         # regex adapted from: https://stackoverflow.com/a/4260512
         choice=$(printf "%s" "$choice" | rg '(([0-9A-Fa-f]{2}[:]){5}[0-9A-Fa-f]{2})' -or '$1')
 
-        notify-send -h ${notif.hint} -i ${notif.icon} "${notif.title}" "Attempting to disconnect $choice" &
+        notify-send -h ${notif.hint} -i ${icon.aquiring} "${notif.title}" "Attempting to disconnect $choice" &
         bluetoothctl disconnect "$choice" > /dev/null
-        notify-send -h ${notif.hint} -i ${notif.icon} -t ${notif.time} "${notif.title}" "Disconnected $choice"
+        notify-send -h ${notif.hint} -i ${icon.disconnected} -t ${notif.time} "${notif.title}" "Disconnected $choice"
       '';
     })
   ];
