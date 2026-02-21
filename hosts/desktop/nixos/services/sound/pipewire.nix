33d8a9bfbba967a99d2ca0d9038a8a3d3e4b5609
EightBitApple <stefanharris16@gmail.com>
2026-02-21 16:16:04 +0000

    Removed pipewire fixed quantum size.

--- hosts/desktop/nixos/services/sound/pipewire.nix
+++ hosts/desktop/nixos/services/sound/pipewire.nix
@@ -1,29 +1,11 @@
-let
-  quantumSize = 256;
-in
-
 {
-  services.pipewire = {
-    extraConfig.pipewire."92-fixed-buffer" = {
-      "context.properties" = {
-        "default.clock.quantum" = quantumSize;
-        "default.clock.min-quantum" = quantumSize;
-        "default.clock.max-quantum" = quantumSize;
-      };
-    };
-
-    wireplumber.extraConfig."99-disable-suspend" = {
-      "monitor.alsa.rules" = [
-        {
-          matches = [
-            { "node.name" = "~alsa_input.*"; }
-            { "node.name" = "~alsa_output.*"; }
-          ];
-          actions.update-props = {
-            "session.suspend-timeout-seconds" = 0;
-          };
-        }
+  services.pipewire.wireplumber.extraConfig."99-disable-suspend"."monitor.alsa.rules" = [
+    {
+      matches = [
+        { "node.name" = "~alsa_input.*"; }
+        { "node.name" = "~alsa_output.*"; }
       ];
-    };
-  };
+      actions.update-props."session.suspend-timeout-seconds" = 0;
+    }
+  ];
 }
