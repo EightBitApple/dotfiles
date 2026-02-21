4c4738a9cd264778c6ceffd65552001eb4fa39f9
EightBitApple <stefanharris16@gmail.com>
2026-02-21 16:12:00 +0000

    Cleaned up fish config.

--- modules/home-manager/packages/utilities/terminal/fish.nix
+++ modules/home-manager/packages/utilities/terminal/fish.nix
@@ -1,14 +1,9 @@
-{ pkgs, userSettings, ... }:
+{ pkgs, ... }:
 
-let
-  cmd_mpv = "mpv --no-video --shuffle --loop-playlist";
-  ytUrl = "https://youtube.com/playlist?list=PL_tDOyt4AsK";
-in
 {
   home.packages = with pkgs; [
     fzf
     fd
-    starship
   ];
 
   programs.fish = {
@@ -17,12 +12,9 @@ in
 
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
-      set fish_user_paths /usr/local/bin /home/${userSettings.user.name}/.local/bin /home/${userSettings.user.name}/.config/emacs/bin
+      set fish_user_paths ~/.config/emacs/bin
       set fish_vi_key_bindings
 
-      starship init fish | source
-
-      # Set up fzf key bindings
       fzf --fish | source
 
       if test (tty) = "/dev/tty1"
@@ -33,27 +25,12 @@ in
     shellAliases = {
       bc = "${pkgs.bc}/bin/bc -l";
       mkdir = "mkdir -pv";
-      ping = "ping -c 5";
       ports = "netstat -tulanp";
-
-      # safety nets
-      mv = "mv -i";
-      cp = "cp -i";
-      ln = "ln -i";
 
       root = "sudo -i";
 
       df = "df -H";
       du = "du -ch";
-
-      # music
-      mus_tracker = "${cmd_mpv} '${ytUrl}c-PxwyDgbpZXWBLGQwDa9u'";
-      mus_tracker_chill = "${cmd_mpv} '${ytUrl}eakyEJjSWTrQIH12h512Iw'";
-      mus_tracker_chip = "${cmd_mpv} '${ytUrl}eyv5d9zFysHFoMR3PeKAo_'";
-      mus_tracker_vg = "${cmd_mpv} '${ytUrl}e645jBv86ivf9w7UjwJ2KE'";
-
-      # program options
-      yt-dlp = "${pkgs.yt-dlp}/bin/yt-dlp --extractor-args \"youtube:player-client=default,-tv_simply\"";
     };
   };
 }
