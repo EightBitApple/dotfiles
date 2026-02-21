c836ce7bfd36aa9d27f9596f172befb95f3c5875
EightBitApple <stefanharris16@gmail.com>
2026-02-21 16:10:47 +0000

    Reverted Swaync version.

--- overlays/hotfixes/swaync.nix
+++ overlays/hotfixes/swaync.nix
@@ -9,8 +9,8 @@ in
       # Commit 5228963: Bumped version to v0.12.4
       owner = "ErikReider";
       repo = "SwayNotificationCenter";
-      rev = "5228963a0dccbee558e153491eb067bd8a053937";
-      hash = "sha256-W4d41+qFT+4Vfpx/v77DuiumQ8mzl6g+PCIJZ+Py1Po=";
+      rev = "aafe626a183daf4105f20c42c68ba14655fbcdcb";
+      hash = "sha256-SYsXzQZGgCuyabP/Ran1qgWCBkmBCLmrqih7bx0VPLs=";
     };
   });
 })
