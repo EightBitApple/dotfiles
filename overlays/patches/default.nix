943fc07a87545fd4b462ab1f28b255e05375c5cc
EightBitApple <stefanharris16@gmail.com>
2026-05-10 19:53:58 +0100

    fixup! Patch gtklock to use bullet char in input field.

--- overlays/patches/default.nix
+++ overlays/patches/default.nix
@@ -2,7 +2,7 @@
   nixpkgs.overlays = [
     (final: prev: {
       gtklock = prev.gtklock.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [ ./bullet-point.patch ];
+        patches = (old.patches or [ ]) ++ [ ./gtklock-bullet-point.patch ];
       });
     })
   ];
