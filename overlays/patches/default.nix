64652265f034a23f8f2ea0a676722a3d75640135
EightBitApple <stefanharris16@gmail.com>
2026-05-11 14:46:21 +0100

    Add icon and label patch to powerbar module.
    The suspend button on the lock screen will now show a different icon.
    Labels are enabled to give the buttons a larger hitbox.

--- overlays/patches/default.nix
+++ overlays/patches/default.nix
@@ -1,8 +1,17 @@
+let
+  pbm = "gtklock-powerbar-module";
+in
 {
   nixpkgs.overlays = [
     (final: prev: {
       gtklock = prev.gtklock.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [ ./gtklock-bullet-point.patch ];
+        patches = (old.patches or [ ]) ++ [ ./gtklock/bullet-point.patch ];
+      });
+    })
+
+    (final: prev: {
+      ${pbm} = prev.${pbm}.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [ ./gtklock/powerbar.patch ];
       });
     })
   ];
